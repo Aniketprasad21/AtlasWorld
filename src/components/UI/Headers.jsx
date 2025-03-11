241c23c29de19cfb1fdfbcd05dbb77872a73b402
Aniketprasad21 <aniketprasad211299@gmail.com>
2025-03-11 17:58:12 +0530

    forth commit

--- src/components/UI/Headers.jsx
+++ src/components/UI/Headers.jsx
@@ -1,46 +1,50 @@
 import React, { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { NavLink } from "react-router-dom";
 
 const Headers = () => {
   const [show, setShow] = useState(false);
-  const handleButtonToggle = () =>{
-  return  setShow(!show);
+  const handleButtonToggle = () => {
+    return setShow(!show);
+  };
+
+  const handleNavLinkClick = () =>{
+    setShow(false)
   }
   return (
     <header>
       <div className="container">
         <div className="grid navbar-grid">
           <div className="Logo">
-            <NavLink to="/">
+            <NavLink to="/" onClick={handleNavLinkClick}>
               <h1>WorldAtlas</h1>
             </NavLink>
           </div>
-          <nav className={show ? "menu-mobile": "menu-web"}>
+          <nav className={show ? "menu-mobile" : "menu-web"}>
             <ul>
               <li>
-                <NavLink to  = "/">Home</NavLink>
+                <NavLink to="/" onClick={handleNavLinkClick}>Home</NavLink>
               </li>
               <li>
-                <NavLink to = "/about">About</NavLink>
+                <NavLink to="/about" onClick={handleNavLinkClick}>About</NavLink>
               </li>
               <li>
-                <NavLink to = "/country">Country</NavLink>
+                <NavLink to="/country" onClick={handleNavLinkClick}>Country</NavLink>
               </li>
               <li>
-                <NavLink to="/contact">FeedBack</NavLink>
+                <NavLink to="/contact" onClick={handleNavLinkClick}>FeedBack</NavLink>
               </li>
               <li>
                 <NavLink></NavLink>
               </li>
             </ul>
           </nav>
-            <div className="ham-menu">
-              <button onClick={handleButtonToggle}>
-                  <GiHamburgerMenu/>
-              </button>
-            </div>
-
+          <div className="ham-menu">
+            <button onClick={handleButtonToggle}>
+              {show ? <AiOutlineClose /> : <GiHamburgerMenu />}
+            </button>
+          </div>
         </div>
       </div>
     </header>
